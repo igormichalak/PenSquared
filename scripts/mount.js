154e09b98012608e1034afb64bdaade5d0043aa2
Igor Michalak <git@igormichalak.com>
2024-08-15 11:06:19 +0200

    Add separate mount script

---
+++ scripts/mount.js
@@ -0,0 +1,21 @@
+window.isPenSquaredActive = true;
+
+const rootEl = document.createElement('div');
+rootEl.id = 'pen-squared-root';
+
+let bgColor = getComputedStyle(document.body).backgroundColor;
+
+const isBgTransparent = bgColor === 'transparent' ||
+                        bgColor === 'rgba(0, 0, 0, 0)';
+
+if (isBgTransparent) {
+    bgColor = 'rgb(255 255 255)';
+}
+
+rootEl.style.backgroundColor = bgColor;
+
+const canvas = document.createElement('canvas');
+rootEl.appendChild(canvas);
+
+document.body.appendChild(rootEl);
+
